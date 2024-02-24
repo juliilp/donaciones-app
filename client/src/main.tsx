138e9ffb560fd51fd4c0ab365528cb86ebb20b42
Julian Lopez Padua <julianlopez43013@gmail.com>
2024-02-23 21:00:06 -0300

    rutas

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Registro from "./vistas/Registro";
+import Login from "./vistas/Login";
+import Home from "./vistas/Home";
+import DashBoard from "./vistas/DashBoard";
+import Perfil from "./vistas/Perfil";
+import axios from "axios";
+axios.defaults.baseURL = "http://localhost:3001";
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/registro" element={<Registro />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<DashBoard />} />
+        <Route path="/perfil/:id" element={<Perfil />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
