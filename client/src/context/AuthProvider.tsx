6eab66d8dbaebd50d71d5bef3d16741eca8f55cd
Julian Lopez Padua <julianlopez43013@gmail.com>
2024-02-25 18:20:07 -0300

    agregue handler Cerrar Sesion y Login

--- client/src/context/AuthProvider.tsx
+++ client/src/context/AuthProvider.tsx
@@ -2,6 +2,8 @@ import React, { createContext, useEffect, useState } from "react";
 import Cookies from "js-cookie";
 import { IAuthProvider } from "../interface/AuthProvider.interface";
 import axios from "axios";
+import { FieldValues, SubmitHandler } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 
 interface AuthProviderProps {
   children: React.ReactNode;
@@ -10,9 +12,9 @@ interface AuthProviderProps {
 export const AuthContext = createContext<IAuthProvider | null>(null);
 
 const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
-  const [user, setUser] = useState<any[]>([]);
+  const [user, setUser] = useState<any>(null);
   const [isAuthenticate, setIsAuthenticate] = useState<boolean>(false);
-
+  const navigate = useNavigate();
   useEffect(() => {
     const cookies = Cookies.get();
     if (cookies.userLoginToken) {
@@ -25,7 +27,7 @@ const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
           setIsAuthenticate(true);
         } catch (error) {
           console.log(error);
-          setUser([]);
+          setUser(null);
           setIsAuthenticate(false);
         }
       }
@@ -33,9 +35,38 @@ const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
     }
   }, []);
 
+  const handlerCerrarSesion = () => {
+    async function cerrarSesion() {
+      await axios("/user/logout");
+      setUser(null);
+      setIsAuthenticate(false);
+    }
+    cerrarSesion();
+  };
+
+  const handlerLogin: SubmitHandler<FieldValues> = async (
+    data: FieldValues
+  ) => {
+    const result = await axios.post(
+      "/user/login",
+      {
+        email: data.email,
+        password: data.password,
+      },
+      { withCredentials: true }
+    );
+    setUser(result.data);
+    setIsAuthenticate(true);
+    if (result.status === 200) {
+      navigate("/");
+    }
+  };
+
   const initialValue: IAuthProvider = {
     user,
     isAuthenticate,
+    handlerCerrarSesion,
+    handlerLogin,
   };
   return (
     <AuthContext.Provider value={initialValue}>{children}</AuthContext.Provider>
