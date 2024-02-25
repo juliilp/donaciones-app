d1c52dd1df12464f60b6c9761be654f4f216ea63
Julian Lopez Padua <julianlopez43013@gmail.com>
2024-02-25 19:42:58 -0300

    boiler plate perfil

--- client/src/vistas/Perfil.tsx
+++ client/src/vistas/Perfil.tsx
@@ -1,5 +1,40 @@
-import React from "react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+import { FieldValues, useForm } from "react-hook-form";
 
 export default function Perfil() {
-  return <div>Perfil</div>;
+  const { register, handleSubmit } = useForm();
+  const navigate = useNavigate();
+  const { isAuthenticate } = useAuth();
+  useEffect(() => {
+    if (!isAuthenticate) {
+      return navigate("/");
+    }
+  }, [isAuthenticate]);
+
+  const handlerSubmit = (data: FieldValues) => {
+    console.log(data);
+  };
+  return (
+    <section>
+      <form onSubmit={handleSubmit(handlerSubmit)}>
+        <input
+          type="text"
+          placeholder="descripcion"
+          {...register("descripcion")}
+        />
+        <input
+          type="text"
+          placeholder="motivoDonacion"
+          {...register("motivoDonacion")}
+        />
+        <input type="text" placeholder="nombre" {...register("nombre")} />
+        <input type="file" {...register("fotoPerfil")} />
+        <button className="block bg-red-500 p-3 text-white rounded-xl">
+          Enviar
+        </button>
+      </form>
+    </section>
+  );
 }
