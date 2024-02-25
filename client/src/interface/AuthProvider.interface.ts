f9c4650c61b6161854fe8bb4180a6acf5b990c5b
Julian Lopez Padua <julianlopez43013@gmail.com>
2024-02-25 18:20:31 -0300

    agregue los tipados de handler cerrar sesion y login

--- client/src/interface/AuthProvider.interface.ts
+++ client/src/interface/AuthProvider.interface.ts
@@ -1,4 +1,19 @@
+import { FieldValues, SubmitHandler } from "react-hook-form";
+
+interface IFoto {
+  public_id: string;
+  secure_url: string;
+}
+
+export interface IUser {
+  email: string;
+  foto: IFoto;
+  nombre: string;
+}
+
 export interface IAuthProvider {
-  user?: any[];
+  user?: IUser;
   isAuthenticate?: boolean;
+  handlerCerrarSesion?: () => void;
+  handlerLogin: SubmitHandler<FieldValues>;
 }
