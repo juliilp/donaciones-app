b62774d89fb26b16db8b715f1f13a17c071317f0
Julian Lopez Padua <julianlopez43013@gmail.com>
2024-02-26 11:28:15 -0300

    interface de estado global

---
+++ client/src/interface/IUserProvider.ts
@@ -0,0 +1,7 @@
+import { FieldValues, SubmitHandler } from "react-hook-form";
+
+export interface IUserProvider {
+  handlerEliminarDatos: (_id: string) => void;
+  handlerCrearDatos: SubmitHandler<FieldValues>;
+  datos: any;
+}
